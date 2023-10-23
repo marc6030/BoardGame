889bc15cf1e41ed238894b361f80e0ad016f5753
SkumJustEatMe <s214662@dtu.dk>
2023-10-23 17:30:08 +0200

    Adding logo

--- app/src/main/java/com/example/myapplication/Frontpage.kt
+++ app/src/main/java/com/example/myapplication/Frontpage.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 class Frontpage {
-
-    fun menu(){
+    @Composable
+    fun LogoImage(modifier: Modifier = Modifier){
+        val image = painterResource(R.drawable.logo)
 
     }
 }
