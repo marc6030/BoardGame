b1cc66c63b993f433da64de91720a1aac4cd9aae
CrazyCows <108196449+CrazyCows@users.noreply.github.com>
2023-11-25 17:17:53 +0100

    Changes by Andersi

--- app/src/main/java/com/example/myapplication/views/FavoriteActivity.kt
+++ app/src/main/java/com/example/myapplication/views/FavoriteActivity.kt
@@ -132,6 +132,7 @@ fun FavoriteActivity(navController: NavHostController, viewModel: MyViewModel) {
                         }
                     }
 
+                }
             }
         }
 
