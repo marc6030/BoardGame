008a7a5118db026f6febe217dbdecea0b8b912fb
Marcus Hansen <mjagdthomsen@gmail.com>
2024-01-04 11:15:29 +0100

    Confetti on top of faverite icen when liking

--- app/src/main/java/com/example/myapplication/views/BoardGameInfoActivity.kt
+++ app/src/main/java/com/example/myapplication/views/BoardGameInfoActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -35,27 +36,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
+import coil.size.Size
 import com.example.myapplication.modelviews.BoardDataViewModel
 import com.example.myapplication.modelviews.FavoriteViewModel
 import com.example.myapplication.modelviews.RatingsViewModel
 import com.example.myapplication.modelviews.SharedViewModel
+import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 
 @Composable
@@ -530,10 +538,11 @@ fun favoriteButton(navController: NavHostController,
                    viewModel: FavoriteViewModel,
                    sharedViewModel: SharedViewModel){
 
+    var triggerConfetti by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth(),
     ) {
         Button(
             onClick = {
@@ -563,10 +572,109 @@ fun favoriteButton(navController: NavHostController,
                 .size(55.dp)
                 .padding(8.dp)
                 .clickable {
+                    triggerConfetti = !triggerConfetti
                     viewModel.toggleFavorite(sharedViewModel.boardGameData!!)
                     Log.v("is still fav", "${sharedViewModel.boardGameData!!.isfavorite}")
                 }
         )
+        if(triggerConfetti){
+            ParticleSystem(18.dp,15.dp,200, modifier = Modifier.align(Alignment.CenterEnd).size(55.dp).padding(8.dp)) // Confetti
+        }
+    }
+}
+
+
+data class Particle(
+    val position: Offset,
+    val velocity: Offset,
+    val acceleration: Offset,
+    val color: Color,
+    val size: Float,
+    val isCircle: Boolean
+)
+
+@Composable
+fun ParticleSystem(posXInDp: Dp, posYInDp: Dp, size: Int, modifier: Modifier) {
+
+    val posX = with(LocalDensity.current) { posXInDp.toPx() }
+    val posY = with(LocalDensity.current) { posYInDp.toPx() }
+
+    // List of particles
+    val particles: MutableList<Particle> = mutableListOf()
+    // For-loop that creates each individual particle and adds it to particles
+    for (i in 1..size) {
+        // Colors that particle can have
+        val colors = listOf(
+            Color(255, 0, 0,255),
+            Color(0, 255, 0,255),
+            Color(0, 0, 255,255),
+            Color(255, 152, 0, 255),
+            Color(255, 235, 59, 255)
+        )
+
+        // Adding the particle to particles
+        particles.add(Particle(
+            Offset(posX.toFloat(), posY.toFloat()),
+            Offset(1F, 1F),
+            Offset(0F, 0F),
+            colors[Random.nextInt(colors.size)],
+            10f,
+            Random.nextBoolean()
+        ))
+    }
+
+    // Add all particles to a mutable state of particles
+    val mutableParticles = remember { mutableStateListOf<Particle>() }
+    mutableParticles.addAll(particles)
+
+    var counter = 0 // Counts how many iterations each particle has been updated
+
+    // Updates each particle pos, vel, acc, size and alpha
+    LaunchedEffect(Unit) {
+        while (true) {
+            val particlesCopy = ArrayList(mutableParticles.map { it.copy() })
+            particlesCopy.forEachIndexed { index, particle ->
+                mutableParticles[index] =
+                    particle.copy(
+                        position = particle.position + particle.velocity,
+                        velocity = particle.velocity + particle.acceleration + Offset(0.0f,0.3f),
+                        acceleration = Offset((Math.random() * 2 - 1).toFloat(), (Math.random() * 2 - 1).toFloat()),
+                        color = particle.color.copy(
+                            red = particle.color.red,
+                            green = particle.color.green,
+                            blue = particle.color.blue,
+                            alpha = (particle.color.alpha + (Math.random() * 5 - 4).toFloat()).coerceIn(0F, 1F)
+                        ),
+                        size = (particle.size + (Math.random() * 2 - 1).toFloat()).coerceIn(8.0f, 12.0f)
+                    )
+
+            }
+
+            delay(16L) // Delay before next iteration
+            counter += 1
+
+            if (counter > 200) {
+                mutableParticles.clear()
+                break
+            }
+        }
+    }
+
+    // Draws the canvas with the particles on
+    Canvas(
+        modifier = modifier
+    ) {
+        mutableParticles.forEach { particle ->
+            if(particle.isCircle){
+                drawCircle(color = particle.color, alpha = 0.6F, center = particle.position, radius = particle.size)
+            } else {
+                drawRect(color = particle.color, alpha = 0.6F, topLeft = particle.position, size = androidx.compose.ui.geometry.Size(
+                    particle.size,
+                    particle.size
+                )
+                )
+            }
+        }
     }
 }
 
